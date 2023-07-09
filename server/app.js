0246e57e90b9cebfa1eb11856ed6b6e310078178
Jaesin Cho <1005jsc@naver.com>
2023-07-10 00:18:46 +0900

    [노드] router 기본  셋팅

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import 'express-async-errors';
+
+import tweetsRouter from './router/tweets.js';
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
+
+app.use('/tweets', tweetsRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
