e867ad4d7ac34b79352f348d620f3b614f99ecd6
Jaesin Cho <1005jsc@naver.com>
2023-07-10 23:01:22 +0900

    [노드] MVC 패턴 적용시키기 - (1) - Model(data)부분 만들기

---
+++ server/data/tweet.js
@@ -0,0 +1,70 @@
+let tweets = [
+  {
+    id: '1',
+    text: '드림코더분들 화이팅',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+  {
+    id: '2',
+    text: '안뇽',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'ellie',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+];
+
+// MVC의 Model(data)부분 옮겨주기
+
+// 내가 어떤 데이터가 있는지는 모델에서 가장 잘 알기때문에
+// 이 로직은  모델에 있어야 한다
+
+// 처음에 이름을 getAllTweets 이렇게 만들고
+// controller 에서 TweetRepository.getAllTweets
+// 이런식으로 불러왔었는데
+// 'Tweet' 이 중복되는 것 같아서 뒤에있는 함수들의 tweets를 전부 뺴버렸다
+
+// TweetRepository.getAllTweets -> TweetRepository.getAll
+
+export const getAll = () => {
+  return tweets;
+};
+
+export const getAllByUsername = (username) => {
+  return tweets.filter((v) => v.username === username);
+};
+
+export const getById = (id) => {
+  return tweets.find((t) => t.id == id);
+};
+
+export const create = (text, name, username) => {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+
+  tweets = [tweet, ...tweets];
+
+  return tweet;
+};
+
+export const update = (id, text) => {
+  const tweet = tweets.find((t) => t.id === id);
+
+  if (tweet) {
+    tweet.text = text;
+  }
+
+  return tweet;
+};
+
+export const remove = (id) => {
+  tweets = tweets.filter((t) => t.id != id);
+};
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,24 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 
-let tweets = [
-  {
-    id: '1',
-    text: '드림코더분들 화이팅',
-    createdAt: Date.now().toString(),
-    name: 'Bob',
-    username: 'bob',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-  {
-    id: '2',
-    text: '안뇽',
-    createdAt: Date.now().toString(),
-    name: 'Bob',
-    username: 'ellie',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-];
+import * as TweetRepository from '../data/tweet.js';
 
 const router = express.Router();
 
@@ -26,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const username = req.query.username;
-  const data = username ? tweets.filter((v) => v.username === username) : tweets;
+  const data = username ? TweetRepository.getAllByUsername(username) : TweetRepository.getAll();
   res.status(200).json(data);
 });
 
@@ -35,7 +18,7 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
 
-  const tweet = tweets.find((t) => t.id == id);
+  const tweet = TweetRepository.getById(id);
 
   if (tweet) {
     res.status(200).json(tweet);
@@ -50,15 +33,9 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
   // 1. 상단에는 필요한 변수들을 정의해줌
   const { text, name, username } = req.body;
-  const tweet = {
-    id: Date.now().toString(),
-    text,
-    createdAt: new Date(),
-    name,
-    username,
-  };
 
-  tweets = [tweet, ...tweets];
+  const tweet = TweetRepository.create(text, name, username);
+
   // 200 : 성공이다, 201 : '성공적으로 만들어졌다'
   res.status(201).json(tweet);
 });
@@ -70,10 +47,9 @@ router.put('/:id', (req, res, next) => {
 
   const text = req.body.text;
 
-  const tweet = tweets.find((t) => t.id === id);
+  const tweet = TweetRepository.update(id, text);
 
   if (tweet) {
-    tweet.text = text;
     res.status(200).json(tweet);
   } else {
     res.status(404).json({ message: `Tweet id(${id}) not found` });
@@ -83,7 +59,7 @@ router.put('/:id', (req, res, next) => {
 // DELETE /tweets/:id
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-  tweets = tweets.filter((t) => t.id != id);
+  TweetRepository.remove(id);
   res.sendStatus(204);
 });
 
