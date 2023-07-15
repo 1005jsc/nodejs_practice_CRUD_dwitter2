be7ff4036aa150b1fc64020c4233974aa0d7ae64
Jaesin Cho <1005jsc@naver.com>
2023-07-15 09:31:21 +0900

    [노드] 유효성 검사 적용

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -2,8 +2,18 @@ import express from 'express';
 import 'express-async-errors';
 
 import * as TweetController from '../controller/tweets.js';
+import { body, validationResult, check } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+// express-validator 로 규칙도 정해보자
+// 유효성 검사는 라우터 쪽에서 해주고 싶다
+
+const validateTweet = [
+  body('text').trim().isLength({ min: 3 }).withMessage('text should be at least 3 characters'),
+  validate,
+];
 
 // GET /tweets , /tweets?username=:username
 
@@ -15,11 +25,16 @@ router.get('/:id', TweetController.getTweet);
 
 // POST /tweets
 
-router.post('/', TweetController.createTweet);
+router.post(
+  '/',
+  validateTweet,
+
+  TweetController.createTweet
+);
 
 // PUT /tweets/:id
 
-router.put('/:id', TweetController.updateTweet);
+router.put('/:id', validateTweet, TweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', TweetController.removeTweet);
